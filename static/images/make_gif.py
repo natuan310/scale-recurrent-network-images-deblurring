50467ad15d3e0479fe3df7f659f270a32824b018
LP670-SV <61509212+natuan310@users.noreply.github.com>
2020-10-26 16:20:16 +0700

    Delete make_gif.py

--- static/images/make_gif.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import os
-IMG_DIR = os.path.join(os.getcwd(),'flask',
-                        'static', 'images', 'gif_cover')
-print(os.listdir(IMG_DIR))
-
-img_lst = [Image.open(os.path.join(IMG_DIR, im)) for im in os.listdir(IMG_DIR)]
-
-img_lst[0].save('gif_cover.gif', save_all=True, append_images=img_lst)
